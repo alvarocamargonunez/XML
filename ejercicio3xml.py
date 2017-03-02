0aa99a61ea3258a0192a421c97cf948f90318c7e
alvarocamargonunez <alvaro-cnz--@hotmail.com>
2017-03-02 08:49:30 +0100

    COMIENZO EJERCICIO 3 XML

---
+++ ejercicio3xml.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*--
+# 3) Introducir el nombre de un club y mostrar su dirección
+from lxml import etree
+from pprint import pprint 
+arbol=etree.parse("/home/alvarocamargo/Descargas/segundo año/lenguaje de marcas/clubpensionistas.xml")
+
