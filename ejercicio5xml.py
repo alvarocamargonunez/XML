3e451e55868834898f073c7150ebb892812ea416
alvarocamargonunez <alvaro-cnz--@hotmail.com>
2017-03-02 12:44:06 +0100

    CONTINUACIÓN EJERCICIO 5 XML

--- ejercicio5xml.py
+++ ejercicio5xml.py
@@ -3,7 +3,7 @@
 #5)Introduce un año y muestre el/los clubes que se han creado ese año.
 from lxml import etree
 
-doc = etree.parse("/home/alvarocamargo/Descargas/segundo año/lenguaje de marcas/clubpensionistas.xml")
+doc = etree.parse("clubpensionistas.xml")
 
 raiz = doc.getroot()
 
@@ -20,4 +20,7 @@ for d in directorio:
 		if t.text.startswith(fecha):
 			
 			nombre=d.findall("nombre")
-			
+			for n in nombre:
+
+				print t.text, n.text
+		
