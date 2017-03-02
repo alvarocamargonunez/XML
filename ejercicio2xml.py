8c207dd757413d1ef51b585f24fae9effdd40e5b
alvarocamargonunez <alvaro-cnz--@hotmail.com>
2017-03-02 08:42:40 +0100

    COMIENZO EJERCICIO2 XML

---
+++ ejercicio2xml.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+# -*-coding: utf-8 -*-
+
+#2)Contar numeros de clubes.
+
+from lxml import etree
+arbol=etree.parse("/home/alvarocamargo/Descargas/segundo año/lenguaje de marcas/clubpensionistas.xml")
