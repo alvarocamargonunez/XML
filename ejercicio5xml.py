aafd9b8d35672c6c08f152848bd955aa40e5cc61
alvarocamargonunez <alvaro-cnz--@hotmail.com>
2017-03-02 12:41:08 +0100

    COMIENZO EJERCICIO 5 XML

---
+++ ejercicio5xml.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*--
+#5)Introduce un año y muestre el/los clubes que se han creado ese año.
+from lxml import etree
+
+doc = etree.parse("/home/alvarocamargo/Descargas/segundo año/lenguaje de marcas/clubpensionistas.xml")
+
+raiz = doc.getroot()
+
+directorio = raiz.findall("club")
