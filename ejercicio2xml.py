ab6ba45a56a642b2907f693a88853cdca7891d64
alvarocamargonunez <alvaro-cnz--@hotmail.com>
2017-03-02 08:43:40 +0100

    CONTINUACION EJERCICIO 2 XML

--- ejercicio2xml.py
+++ ejercicio2xml.py
@@ -5,3 +5,13 @@
 
 from lxml import etree
 arbol=etree.parse("/home/alvarocamargo/Descargas/segundo año/lenguaje de marcas/clubpensionistas.xml")
+
+raiz=arbol.getroot()
+datos=raiz.findall("club")
+
+
+for a in datos:
+	nombre=a.findall("nombre")
+	for b in nombre:
+		
+		
