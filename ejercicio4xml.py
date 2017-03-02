9027b783b4ce7d0a87a8277b92e295b20500079a
alvarocamargonunez <alvaro-cnz--@hotmail.com>
2017-03-02 12:40:00 +0100

    FIN, FUNCIONA CORRECTAMENTE

--- ejercicio4xml.py
+++ ejercicio4xml.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*-coding: utf-8 -*-
-
+#4)Introduce una subcadena , en el caso que coincida con un numero de teléfono muestre de que club se trata.
 from lxml import etree
 
 doc = etree.parse("/home/alvarocamargo/Descargas/segundo año/lenguaje de marcas/clubpensionistas.xml")
@@ -10,7 +10,7 @@ raiz = doc.getroot()
 directorio = raiz.findall("club")
 
 var = raw_input("Subcadena de un numero de telefono: ")
-
+hay=False
 for d in directorio:
 	telefono= d.findall("telefono")
 	
@@ -18,5 +18,10 @@ for d in directorio:
 		
 		
 		if c.text.startswith(var):
+			hay=True
 			nombre=d.findall("nombre")
-	
+			for y in nombre:
+
+				print c.text, y.text
+if not hay:
+	print "No hay ningún número de telefono que empiece por dicha cadena"
