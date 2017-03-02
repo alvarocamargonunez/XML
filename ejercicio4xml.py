b8fb4e27f7e7514ac24667cb336f8f0eba6e744c
alvarocamargonunez <alvaro-cnz--@hotmail.com>
2017-03-02 12:05:19 +0100

    CONTINUACIÓN EJERCICIO 4 XML

--- ejercicio4xml.py
+++ ejercicio4xml.py
@@ -7,4 +7,14 @@ doc = etree.parse("/home/alvarocamargo/Descargas/segundo año/lenguaje de marcas
 
 raiz = doc.getroot()
 
+directorio = raiz.findall("club")
 
+var = raw_input("Subcadena de un numero de telefono: ")
+
+for d in directorio:
+	telefono= d.findall("telefono")
+	
+	for c in telefono:
+		
+		
+		
